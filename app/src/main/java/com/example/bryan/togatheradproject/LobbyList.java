c3c996ac40aad179216e1830a1738ef2a2f7d9bf
sukibeww <42060507+sukibeww@users.noreply.github.com>
2018-08-03 22:17:14 +0800

    added LobbyList java class Added lobby_list_layout.xml

---
+++ app/src/main/java/com/example/bryan/togatheradproject/LobbyList.java
@@ -0,0 +1,37 @@
+package com.example.bryan.togatheradproject;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class LobbyList extends ArrayAdapter<Lobby> {
+    private Activity context;
+    private List<Lobby> lobbyList;
+
+    public LobbyList(Activity context, List<Lobby> lobbyList) {
+        super(context, R.layout.lobby_list_layout, lobbyList);
+        this.context = context;
+        this.lobbyList = lobbyList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        View listViewItem = inflater.inflate(R.layout.lobby_list_layout, null, true);
+        TextView textViewHostID = (TextView) listViewItem.findViewById(R.id.textView_LobbyListLayout_hostID);
+        TextView textViewLocation = (TextView) listViewItem.findViewById(R.id.textView_LobbyListLayout_location);
+        TextView textViewActivity = (TextView) listViewItem.findViewById(R.id.textView_LobbyListLayout_activity);
+        Lobby lobby = lobbyList.get(position);
+
+        return listViewItem;
+    }
+}
