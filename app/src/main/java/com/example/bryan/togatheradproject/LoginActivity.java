c1fe3725ffb899d55152c188cecd99f7a4f6b82c
sukibeww <42060507+sukibeww@users.noreply.github.com>
2018-08-06 20:34:48 +0800

    Firestore create lobby Function (Recommit)

--- app/src/main/java/com/example/bryan/togatheradproject/LoginActivity.java
+++ app/src/main/java/com/example/bryan/togatheradproject/LoginActivity.java
@@ -38,6 +38,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
                 startActivity(intent);
+
             }
         });
 
