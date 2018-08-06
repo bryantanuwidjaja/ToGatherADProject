d5d9ef53f2e5251b8fe2d3c34bf468c82d035bc8
Bryan <bryantanuwijaya12@gmail.com>
2018-08-06 20:50:34 +0800

    Correction!

--- app/src/main/java/com/example/bryan/togatheradproject/CreateLobbyActivity.java
+++ app/src/main/java/com/example/bryan/togatheradproject/CreateLobbyActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,6 +18,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
+
 
 
 public class CreateLobbyActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
--- app/src/main/java/com/example/bryan/togatheradproject/LoginActivity.java
+++ app/src/main/java/com/example/bryan/togatheradproject/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.example.bryan.togatheradproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 
 public class LoginActivity extends AppCompatActivity {
 
