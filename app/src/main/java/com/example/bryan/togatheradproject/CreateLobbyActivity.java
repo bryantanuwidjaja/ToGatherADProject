045737f42e6b6fd3f459f88b899ed013d143c2e7
Bryan <bryantanuwijaya12@gmail.com>
2018-08-06 20:30:06 +0800

    Revert "Merge branch 'Suki'"
    This reverts commit 35d44626dea2ba61d1f8d9e3851a91bab7b3e559, reversing
    changes made to 1a43bc9048189430004cfc69c621707983df1fdd.

--- app/src/main/java/com/example/bryan/togatheradproject/CreateLobbyActivity.java
+++ app/src/main/java/com/example/bryan/togatheradproject/CreateLobbyActivity.java
@@ -5,7 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,6 +20,7 @@ import android.widget.Toast;
 
 
 public class CreateLobbyActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
 
     private static final String TAG = "CreateLobbyActivity";
     TextView textView_Activity;
