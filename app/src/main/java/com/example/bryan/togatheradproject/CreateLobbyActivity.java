2be1cd70fe783a8635759f35b68b17001dc504d2
Devanw <devan9e03@ymail.com>
2018-08-06 18:39:23 +0800

    added methods to list activities in create lobby spinner, added array in string resource

--- app/src/main/java/com/example/bryan/togatheradproject/CreateLobbyActivity.java
+++ app/src/main/java/com/example/bryan/togatheradproject/CreateLobbyActivity.java
@@ -2,12 +2,35 @@ package com.example.bryan.togatheradproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class CreateLobbyActivity extends AppCompatActivity {
+public class CreateLobbyActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_lobby);
+
+        Spinner spinner = findViewById(R.id.spinnerActivities);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.activities, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item );
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();;
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
