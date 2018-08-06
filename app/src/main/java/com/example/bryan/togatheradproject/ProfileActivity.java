7616fc59c751b534b7d11a74309a5dafc1450025
Joycelind <joycelindchow@yahoo.co.id>
2018-08-06 13:08:41 +0800

    refractoring InterestActivity and ProfileActivity

--- app/src/main/java/com/example/bryan/togatheradproject/ProfileActivity.java
+++ app/src/main/java/com/example/bryan/togatheradproject/ProfileActivity.java
@@ -3,11 +3,11 @@ package com.example.bryan.togatheradproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ProfileScreen extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_screen);
+        setContentView(R.layout.activity_profile);
     }
 }
