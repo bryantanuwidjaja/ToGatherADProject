c9f0bccd5c9c122f6d56f8e7ba5c451eae1dee61
Joycelind <joycelindchow@yahoo.co.id>
2018-08-07 14:10:15 +0800

    Added Intent

--- app/src/main/java/com/example/bryan/togatheradproject/HomeActivity.java
+++ app/src/main/java/com/example/bryan/togatheradproject/HomeActivity.java
@@ -1,8 +1,10 @@
 package com.example.bryan.togatheradproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,6 +18,7 @@ public class HomeActivity extends AppCompatActivity {
     ListView listView_LobbyList;
     TextView textView_Nearbylobby;
     Button button_Createlobby;
+    Button button_Viewprofile;
 
 
     @Override
@@ -28,9 +31,24 @@ public class HomeActivity extends AppCompatActivity {
         listView_LobbyList = findViewById(R.id.listView_HomeActivity_lobbyList);
         textView_Nearbylobby = findViewById(R.id.textView_HomeActivity_nearbyLobby);
         button_Createlobby = findViewById(R.id.button_HomeActivity_createLobby);
+        button_Viewprofile = findViewById(R.id.button_HomeActivity_viewProfile);
 
         Log.d(TAG, "onCreate: out");
 
+        button_Createlobby.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CreateLobbyActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        button_Viewprofile.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent (getApplicationContext(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/bryan/togatheradproject/RegistrationActivity.java
+++ app/src/main/java/com/example/bryan/togatheradproject/RegistrationActivity.java
@@ -1,5 +1,6 @@
 package com.example.bryan.togatheradproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
