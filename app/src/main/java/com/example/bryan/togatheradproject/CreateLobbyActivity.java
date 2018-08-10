d833a0524f0defdfaedaa19d73ed4d852bff7fe9
Devanw <devan9e03@ymail.com>
2018-08-10 21:41:50 +0800

    added function to get the current location's address

--- app/src/main/java/com/example/bryan/togatheradproject/CreateLobbyActivity.java
+++ app/src/main/java/com/example/bryan/togatheradproject/CreateLobbyActivity.java
@@ -1,14 +1,28 @@
 package com.example.bryan.togatheradproject;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.ResultReceiver;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -33,6 +47,7 @@ public class CreateLobbyActivity extends AppCompatActivity implements AdapterVie
 
     }
 
+
     private static final String TAG = "CreateLobbyActivity";
     TextView textView_Activity;
     TextView textView_Capacity;
@@ -46,6 +61,7 @@ public class CreateLobbyActivity extends AppCompatActivity implements AdapterVie
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_lobby);
+
 
         textView_Activity = findViewById(R.id.textView_CreateLobbyActivity_activity);
         textView_Capacity = findViewById(R.id.textView_CreateLobbyActivity_capacity);
@@ -100,5 +116,273 @@ public class CreateLobbyActivity extends AppCompatActivity implements AdapterVie
             }
         });
 
+        //2nd part to install fetch address button
+        mResultReceiver = new AddressResultReceiver(new Handler());
+
+        mLocationAddressTextView = (TextView) findViewById(R.id.textView_CreateLobbyActivity_address);
+        mFetchAddressButton = (Button) findViewById(R.id.button_CreateLobbyActivity_fetch);
+
+        // Set defaults, then update using values stored in the Bundle.
+        mAddressRequested = false;
+        mAddressOutput = "";
+        updateValuesFromBundle(savedInstanceState);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        updateUIWidgets();
+
     }
+
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+
+    private static final String ADDRESS_REQUESTED_KEY = "address-request-pending";
+    private static final String LOCATION_ADDRESS_KEY = "location-address";
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location mLastLocation;
+    private boolean mAddressRequested;
+    private String mAddressOutput;
+    private AddressResultReceiver mResultReceiver;
+    private TextView mLocationAddressTextView;
+    private Button mFetchAddressButton;
+
+    private boolean checkPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        boolean shouldProvideRationale =
+                ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION);
+
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.");
+
+            showSnackbar(R.string.permission_rationale, android.R.string.ok,
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            // Request permission
+                            ActivityCompat.requestPermissions(CreateLobbyActivity.this,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    REQUEST_PERMISSIONS_REQUEST_CODE);
+                        }
+                    });
+
+        } else {
+            Log.i(TAG, "Requesting permission");
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the user denied the permission
+            // previously and checked "Never ask again".
+            ActivityCompat.requestPermissions(CreateLobbyActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        Log.i(TAG, "onRequestPermissionResult");
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.length <= 0) {
+                // If user interaction was interrupted, the permission request is cancelled and you
+                // receive empty arrays.
+                Log.i(TAG, "User interaction was cancelled.");
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted.
+                getAddress();
+            } else {
+                // Permission denied.
+
+                // Notify the user via a SnackBar that they have rejected a core permission for the
+                // app, which makes the Activity useless. In a real app, core permissions would
+                // typically be best requested during a welcome-screen flow.
+
+                // Additionally, it is important to remember that a permission might have been
+                // rejected without asking the user for permission (device policy or "Never ask
+                // again" prompts). Therefore, a user interface affordance is typically implemented
+                // when permissions are denied. Otherwise, your app could appear unresponsive to
+                // touches or interactions which have required permissions.
+                showSnackbar(R.string.permission_denied_explanation, R.string.settings,
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                // Build intent that displays the App settings screen.
+                                Intent intent = new Intent();
+                                intent.setAction(
+                                        Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                Uri uri = Uri.fromParts("package",
+                                        BuildConfig.APPLICATION_ID, null);
+                                intent.setData(uri);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        });
+            }
+        }
+    }
+
+
+    private void showSnackbar(final String text) {
+        View container = findViewById(android.R.id.content);
+        if (container != null) {
+            Snackbar.make(container, text, Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    private void showSnackbar(final int mainTextStringId, final int actionStringId,
+                              View.OnClickListener listener) {
+        Snackbar.make(findViewById(android.R.id.content),
+                getString(mainTextStringId),
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(getString(actionStringId), listener).show();
+    }
+
+    private void updateValuesFromBundle(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            // Check savedInstanceState to see if the address was previously requested.
+            if (savedInstanceState.keySet().contains(ADDRESS_REQUESTED_KEY)) {
+                mAddressRequested = savedInstanceState.getBoolean(ADDRESS_REQUESTED_KEY);
+            }
+            // Check savedInstanceState to see if the location address string was previously found
+            // and stored in the Bundle. If it was found, display the address string in the UI.
+            if (savedInstanceState.keySet().contains(LOCATION_ADDRESS_KEY)) {
+                mAddressOutput = savedInstanceState.getString(LOCATION_ADDRESS_KEY);
+                displayAddressOutput();
+            }
+        }
+    }
+
+    private void updateUIWidgets() {
+        if (mAddressRequested) {
+            mFetchAddressButton.setEnabled(false);
+        } else {
+            mFetchAddressButton.setEnabled(true);
+        }
+    }
+    public void fetchAddressButtonHandler(View view) {
+        Log.d(TAG, "fetchAddressButtonHandler: startbutton");
+        if (mLastLocation != null) {
+            Log.d(TAG, "fetchAddressButtonHandler: its in if");
+            startIntentService();
+            Log.d(TAG, "fetchAddressButtonHandler: finish intent");
+            return;
+        }
+        Log.d(TAG, "fetchAddressButtonHandler: if done");
+        // If we have not yet retrieved the user location, we process the user's request by setting
+        // mAddressRequested to true. As far as the user is concerned, pressing the Fetch Address button
+        // immediately kicks off the process of getting the address.
+        mAddressRequested = true;
+        Log.d(TAG, "fetchAddressButtonHandler: abs");
+        updateUIWidgets();
+        Log.d(TAG, "fetchAddressButtonHandler: last");
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (!checkPermissions()) {
+            requestPermissions();
+        } else {
+            getAddress();
+        }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private void getAddress() {
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location == null) {
+                            Log.w(TAG, "onSuccess:null");
+                            return;
+                        }
+
+                        mLastLocation = location;
+
+                        // Determine whether a Geocoder is available.
+                        if (!Geocoder.isPresent()) {
+                            showSnackbar(getString(R.string.no_geocoder_available));
+                            return;
+                        }
+
+                        // If the user pressed the fetch address button before we had the location,
+                        // this will be set to true indicating that we should kick off the intent
+                        // service after fetching the location.
+                        if (mAddressRequested) {
+                            startIntentService();
+                        }
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "getLastLocation:onFailure", e);
+                    }
+                });
+    }
+    private void startIntentService() {
+        // Create an intent for passing to the intent service responsible for fetching the address.
+        Log.d(TAG, "startIntentService: fetchcreateintent");
+        Intent intent = new Intent(this, FetchAddressIntentService.class);
+
+        // Pass the result receiver as an extra to the service.
+        intent.putExtra("com.example.bryan.togatheradproject.RECEIVER", mResultReceiver);
+
+        // Pass the location data as an extra to the service.
+        intent.putExtra("com.example.bryan.togatheradproject.LOCATION_DATA_EXTRA", mLastLocation);
+        Log.d(TAG, "startIntentService: fetchbeforeintent");
+        // Start the service. If the service isn't already running, it is instantiated and started
+        // (creating a process for it if needed); if it is running then it remains running. The
+        // service kills itself automatically once all intents are processed.
+        startService(intent);
+    }
+
+    private void displayAddressOutput() {
+        mLocationAddressTextView.setText(mAddressOutput);
+    }
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save whether the address has been requested.
+        savedInstanceState.putBoolean(ADDRESS_REQUESTED_KEY, mAddressRequested);
+
+        // Save the address string.
+        savedInstanceState.putString(LOCATION_ADDRESS_KEY, mAddressOutput);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    private class AddressResultReceiver extends ResultReceiver {
+        AddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+
+        /**
+         *  Receives data sent from FetchAddressIntentService and updates the UI in MainActivity.
+         */
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+
+            // Display the address string or an error message sent from the intent service.
+            mAddressOutput = resultData.getString("com.example.bryan.togatheradproject.RECEIVER");
+            displayAddressOutput();
+
+            // Show a toast message if an address was found.
+            if (resultCode == 0) {
+                showToast("Address Found");
+            }
+
+            // Reset. Enable the Fetch Address button and stop showing the progress bar.
+            mAddressRequested = false;
+            updateUIWidgets();
+        }
+    }
+
 }
---
+++ app/src/test/java/com/example/bryan/togatheradproject/CheckNewUserIsInDatabase.java
@@ -0,0 +1,38 @@
+package com.example.bryan.togatheradproject;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class CheckNewUserIsInDatabase {
+    private String expectedEmail = "dummy1@dummy.com";
+    private String expectedUsername = "dummy1";
+    private String expectedPassword = "nopassword";
+    private String wrongPassword = "password";
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuthInvalidCredentialsException firebaseAuthInvalidCredentialsException;
+
+    @Test
+    public void isUserRegistered(){
+//        mAuth.createUserWithEmailAndPassword(expectedEmail, expectedPassword);
+    }
+
+    @Test
+    public void registerWithWrongPassword(){
+        RegistrationActivity checkPassword = new RegistrationActivity();
+        //Assert.assertEquals(checkPassword.checkIfPasswordSame(expectedPassword, wrongPassword), false);
+    }
+
+    @Test
+    public void registerWithCorrectPassword(){
+        RegistrationActivity checkPassword = new RegistrationActivity();
+        //Assert.assertEquals(checkPassword.checkIfPasswordSame(expectedPassword, expectedPassword), true);
+    }
+
+
+
+}
